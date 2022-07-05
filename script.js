f0740ba630d1c9802c4b3c55244157d1c04d54d1
Jonas Paiva <jonaspwork@gmail.com>
2022-07-05 05:03:32 -0300

    requisito 1 até o 6

--- script.js
+++ script.js
@@ -4,8 +4,46 @@ function colorPalette() {
   for (let index = 0; index < 4; index += 1) {
     const DivColor = document.getElementById(`${index}`);
     DivColor.style.backgroundColor = colorArray[index];
-    console.log(DivColor);
   }
 }
 
 colorPalette();
+
+function criandoPixel() {
+  const pixelBoard = document.getElementById('pixel-board');
+
+  const DivPixel = document.createElement('div');
+
+  pixelBoard.appendChild(DivPixel).className = 'pixel';
+
+  console.log('teste');
+}
+criandoPixel();
+criandoPixel();
+criandoPixel();
+criandoPixel();
+criandoPixel();
+
+criandoPixel();
+criandoPixel();
+criandoPixel();
+criandoPixel();
+criandoPixel();
+
+criandoPixel();
+criandoPixel();
+criandoPixel();
+criandoPixel();
+criandoPixel();
+
+criandoPixel();
+criandoPixel();
+criandoPixel();
+criandoPixel();
+criandoPixel();
+
+criandoPixel();
+criandoPixel();
+criandoPixel();
+criandoPixel();
+criandoPixel();

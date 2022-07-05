3417d8025d8f54060ace972ef072bce1c90b266e
Jonas Paiva <jonaspwork@gmail.com>
2022-07-05 02:56:50 -0300

    requisito 2 e 3

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function colorPalette() {
+  const colorArray = ['black', 'blue', 'red', 'yellow'];
 
+  for (let index = 0; index < 4; index += 1) {
+    const DivColor = document.getElementById(`${index}`);
+    DivColor.style.backgroundColor = colorArray[index];
+    console.log(DivColor);
+  }
+}
+
+colorPalette();
